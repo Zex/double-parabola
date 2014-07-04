0debe0054315282bdd2060b436c433cdfd9f52ec
Zex <top_zlynch@yahoo.com>
2014-07-04 11:03:28 +0800

    add FlexablePainter

--- DoubleParabola.h
+++ DoubleParabola.h
@@ -18,7 +18,6 @@ typedef std::vector<CvPoint> Points;
 class SingleParabola
 {
     int step_x_;
-    unsigned int points_count_;
     float a_, b_, c_; 
     Points curve_points_;
     CvPoint start_, end_, vertex_;
@@ -33,10 +32,18 @@ public:
     {   
     }   
 
+	unsigned int Resize();
+
     void StepX(int step)
     {   
         step_x_ = step;
+		Resize();
     }
+
+	int StepX()
+	{
+		return step_x_;
+	}
     
     Points& CurvePoints()
     {
@@ -44,19 +51,34 @@ public:
         return ret;
     }
 
-    CvPoint StartPt()
+    void StartPt(CvPoint start)
+	{
+		start_ = start;
+		Resize();
+	}
+
+    CvPoint& StartPt()
     {
-        return start_;
+		CvPoint &ret = start_;
+        return ret;
     }
 
-    CvPoint EndPt()
+	void EndPt(CvPoint end)
+	{
+		end_ = end;
+		Resize();
+	}
+
+    CvPoint& EndPt()
     {
-        return end_;
+		CvPoint &ret = end_;
+        return ret;
     }
 
-    CvPoint VertexPt()
+    CvPoint& VertexPt()
     {
-        return vertex_;
+		CvPoint &ret = vertex_;
+        return ret;
     }
 
     CvPoint VertexPt(CvPoint vertex)
@@ -74,8 +96,7 @@ class DoubleHalfParabola : public SingleParabola
     CvPoint mid1_, mid2_;
 
 public:
-
-    typedef SingleParabola base;
+	typedef SingleParabola base;
 
     DoubleHalfParabola(CvPoint start, CvPoint end, CvPoint vertex, int step = 1);
 
@@ -84,12 +105,42 @@ public:
     }
 
     void GenFullCurve();
+
 	void VertexPt(CvPoint vertex);
+	CvPoint VertexPt()
+	{
+		return base::VertexPt();
+	}
 
     Points FullCurve()
     {
         return CurvePoints();
     }
 };
+
+//------------------------------------------------------------------
+
+class FlexablePainter : public DoubleHalfParabola
+{
+	IplImage *image_;
+
+public:
+	typedef DoubleHalfParabola base;
+
+	FlexablePainter(IplImage *image, CvPoint start, CvPoint end, CvPoint vertex, int step_x = 1)
+	: DoubleHalfParabola(start, end, vertex, step_x), image_(image)
+	{
+	}
+
+	~FlexablePainter()
+	{
+	}
+
+	void VertexPt(CvPoint vertex);
+	void PaintFlexable();
+	void PaintCross(CvPoint center);
+};
+
+
 
 #endif /* __DOUBLE_PARABOLA_H_ */
